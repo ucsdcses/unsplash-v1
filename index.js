2f985a6494c2b5b535e7533efec37b40911f93a4
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-10-16 17:57:51 -0700

    initial commit with code

---
+++ index.js
@@ -0,0 +1,89 @@
+import multer, {memoryStorage} from "multer";
+import express from "express";
+import bodyParser from 'body-parser';
+import {Storage} from "@google-cloud/storage";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// configure dotenv so as to fetch environment variables from the .env file
+const googleCloudStorage = new Storage({
+  projectId: process.env.GCLOUD_STORAGE_PROJECT,
+  keyFilename: process.env.GCLOUD_KEY_FILE
+});
+
+const app = express(); // start a server
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Multer is required to upload files from the client side and make it available in req.files
+const m = multer({
+  storage: memoryStorage(),
+  limits: {
+    fileSize: 4 * 1024 * 1024 // no larger than 4mb
+  }
+});
+
+// A bucket is like a folder (directory to store files/images)
+const bucket = googleCloudStorage.bucket(GCLOUD_STORAGE_BUCKET);
+
+// Helper function to retrieve the image url of a file uploaded to google cloud storage
+function getPublicUrl (bucket, blob) {
+  // template string syntax equivalent: printf ("https://storage.googleapis.com/%s/%s" bucket.name, blob.name)
+  return `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
+}
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+ 
+});
+
+/**
+ * @api /upload
+ * @post Post a file from the client to node.js and then store it in cloud storage and send the url back to the client
+ */
+app.post("/upload", m.single("file"), (req, res, next) => {
+  // the file is available in req.file
+  console.debug('File uploaded by user: ', req.file);
+
+  if (!req.file) {
+    res.status(400).send("No file uploaded.");  // 400 is an error code. Link: https://www.restapitutorial.com/httpstatuscodes.html
+    return;
+  }
+
+  // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);    // originalname is a property available through the user upload itself
+
+  // Make sure to set the contentType metadata for the browser to be able
+  // to render the image instead of downloading the file (default behavior)
+  // Reference Link: https://cloud.google.com/nodejs/getting-started/using-cloud-storage#upload_to_cloud_storage
+  const blobStream = blob.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype // https://stackoverflow.com/questions/3828352/what-is-a-mime-type
+    }
+  });
+
+  blobStream.on("error", err => { // event handler for when an error occurs
+    next(err);
+    return;
+  });
+
+  blobStream.on("finish", () => {
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = getPublicUrl(bucket, blob);
+
+    // Make the image public to the web (since we'll be displaying it in browser)
+    blob.makePublic().then(() => {
+      console.log(`File successfully uploaded to: ${publicUrl}`);
+      res.status(200).send([publicUrl]);
+    });
+
+  });
+
+  blobStream.end(req.file.buffer);  // end the stream
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
